4f6156f7b9e3330c3b026832c5a9d00c7c6217ab
Vinicius16109 <vinicius.golinski@escola.pr.gov.br>
2026-05-26 18:08:31 -0300

    Aula2

---
+++ ex01.js
@@ -0,0 +1,8 @@
+//ex01
+const input = require("readline-sync")
+
+let nome = input.question("Nome: ")
+let idade = input.question("Idade: ")
+let cidade = input.question("Cidade: ")
+
+console.log(`\nNome: ${nome}\nIdade: ${idade}\nCidade: ${cidade}`)
---
+++ ex04.js
@@ -0,0 +1,8 @@
+//ex04
+const input = require("readline-sync")
+
+let numero = parseFloat(input.question("Número: "))
+
+for (let i = 0; i <= 10; i++) {
+    console.log(`${numero} x ${i} = ${numero * i}`)
+}
